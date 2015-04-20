0109215944f61b534c59e2dd31b55c45a47334ab
Denis Aleksandrov <denal05@gmail.com>
2015-04-20 14:14:20 +0200

    Image Views

---
+++ Image Views/Image Views/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Image Views
+//
+//  Created by Denis Aleksandrov on 4/20/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var backgroundImage: UIImageView!
+    @IBOutlet var myLabel: UILabel!
+    
+    var imageList: [String] = ["palms.jpg","mountain.jpg","newyork.jpg"]
+    var nameList:  [String] = ["Beach Vacation", "Hike", "Big Apple"]
+    
+    let maxImages = 2
+    var imageIndex: NSInteger = 1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        backgroundImage.image = UIImage(named: "palms.jpg")
+        myLabel.text = nameList[0]
+        
+    }
+
+    @IBAction func nextImage() {
+        if imageIndex > maxImages {
+            imageIndex = 0
+        }
+        
+        backgroundImage.image = UIImage(named:imageList[imageIndex])
+        myLabel.text = nameList[imageIndex]
+        imageIndex++
+    }
+        
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
